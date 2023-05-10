422fdf85fa1f913420b6f963b39ac5be8928d765
sontv8 <sontv8@fpt.edu.vn>
2023-05-10 08:25:40 +0700

    first commit

---
+++ Day01/main.js
@@ -0,0 +1,46 @@
+/*
+    Biến
+    - Từ khóa khai báo biến: var, let, const
+*/
+// var myName
+// console.log(myName);
+// var myName = "sontv"
+
+// var myName;
+// console.log(myName);
+// myName = "sontv"
+
+// var myName = "sontv"
+// if (true) {
+//     var myName = "thienth"
+// }
+// console.log(myName);
+
+// const myName = "sontv"
+// myName = "thienth"
+// // if (true) {
+// //     const myName = "thienth"
+// // }
+// console.log(myName);
+
+// const myNumber = [1, 2, 3]
+// myNumber = [2, 3, 4, 5]
+// myNumber[0] = 1000;
+// console.log(myNumber[0]);
+
+const myInfo = {
+    id: 1,
+    name: "sontv",
+}
+myInfo.id = 10;
+// myInfo = {
+//     id: 10,
+//     name: "thienth"
+// }
+console.log(myInfo);
+
+/*
+    - toán tử
+    - vòng lặp
+    - câu lệnh rẽ nhánh (if else)
+*/
